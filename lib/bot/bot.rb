a26a2a384688909b247a4d12d13e6be2c48dca4d
Peter Johnstone <peter.wdj@gmail.com>
2018-01-09 15:24:42 +0000

    Leave comments explaining the play method

--- lib/bot/bot.rb
+++ lib/bot/bot.rb
@@ -3,6 +3,10 @@ class Bot
     @engine = Engine.new
   end
 
+  # Field is the Game's @status object, which is an instance of the Status class
+  # Time is an integer, representing the number of milliseconds we have to make
+  # the move
+  # This method is where the strategy for the bot is going to take place
   def play(field, time)
     return 'Random Winning Move'
   end
